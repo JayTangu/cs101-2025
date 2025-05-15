3676a1043c1000113d8d28cb3d49dd7f48c9a298
JayTangu <yade0720@gmail.com>
2025-05-15 15:13:33 +0800

    Update main.cpp

--- new_ex/main.cpp
+++ new_ex/main.cpp
@@ -9,16 +9,10 @@ struct Student {
 
 int main() {
 	Student* p1 = new Student[5];
-	p1[0].id=1;
-	p1[1].id=2;
-	p1[2].id=3;
-	p1[3].id=4;
-	p1[4].id=5;
-	p1[0].name="IU1";
-	p1[1].name="IU2";
-	p1[2].name="IU3";
-	p1[3].name="IU4";
-	p1[4].name="IU5";
+	for(int i = 0 ; i < 5 ; i++) {
+		p1[i].id = 1+i;
+		p1[i].name="IU"+to_string(i+1);
+	}
 	for(int i = 0 ; i < 5 ; i++) {
 		cout<<"array("<<i<<") "<<p1[i].id<<", "<<p1[i].name<<endl;
 	}
